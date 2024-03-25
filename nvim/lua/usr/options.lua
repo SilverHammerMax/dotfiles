568e39f410a519a9bb213bab6be11db50213dafd
SilverHammerMax <github@flibig.com>
2024-03-24 23:31:11 -0400

    Initial Commit

---
+++ nvim/lua/usr/options.lua
@@ -0,0 +1,13 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.autoindent = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.g.mapleader = ";"
+
+vim.keymap.set('n', '<Leader>w', '<cmd>write<cr>', {desc = 'Write'})
+vim.keymap.set('n', '<Leader>q', '<cmd>quit<cr>', {desc = 'Quit'})
