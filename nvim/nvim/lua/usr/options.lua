046c26855a9e8538f2851745ce54d533a174de9a
SilverHammerMax <157190910+SilverHammerMax@users.noreply.github.com>
2025-03-25 20:30:37 -0400

    Long overdue update

---
+++ nvim/nvim/lua/usr/options.lua
@@ -0,0 +1,18 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.autoindent = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.cmd.highlight("Normal guibg=none")
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.g.mapleader = ";"
+
+vim.keymap.set('n', '<Leader>w', '<cmd>write<cr>', {desc = 'Write'})
+vim.keymap.set('n', '<Leader>q', '<cmd>quit<cr>', {desc = 'Quit'})
